00ccdec662fd483744c7b99545c2f963ab33e96c
joanaccgomes <a63577@alunos.ipb.pt>
2026-03-20 11:34:04 +0000

    Cube Aletrações no Script

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 1);
+        transform.localScale = Vector3.one * 2.5f; //Cheaged Scale
+        
+        Material material = Renderer.material;
+        
+        material.color = new Color(1.0f, 0.5f, 0.3f, 1.0f); //Changed Color
+    }
+    
+    void Update()
+    {
+        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+    }
+}
